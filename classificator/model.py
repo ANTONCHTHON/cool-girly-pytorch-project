4f0913b5522cc82ec49bd33b3a0fef55118d641f
emoboy <pumpkin.kate@yandex.ru>
2023-10-31 19:29:33 +0300

    corrected model

--- classificator/model.py
+++ classificator/model.py
@@ -11,13 +11,13 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(6,16,5)
         self.fc1 = nn.Linear(16*5*5,120)
         self.fc2 = nn.Linear(120,84)
-        self.fc3 = nn.Linear(84*10)
+        self.fc3 = nn.Linear(84,10)
 
     def forward(self,x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x,1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
